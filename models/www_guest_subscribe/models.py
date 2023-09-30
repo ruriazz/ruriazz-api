07ea0b8f2d3199b8779466405b9649b36206936f
ruriazz <azizruri.s@gmail.com>
2023-10-01 02:32:43 +0700

    fix uuid error on vercel

--- models/www_guest_subscribe/models.py
+++ models/www_guest_subscribe/models.py
@@ -1,10 +1,8 @@
-import uuid
 from django.db import models
-from django.utils import timezone
 from models import BaseModelManager
 
 class WWWGuestSubscribe(models.Model):
-    uid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id = models.BigAutoField(primary_key=True)
     email = models.EmailField(unique=True, null=False, blank=False)
     topics = models.JSONField(null=False, blank=False, default=list)
     created_at = models.DateTimeField(auto_now_add=True, null=False, blank=False)
